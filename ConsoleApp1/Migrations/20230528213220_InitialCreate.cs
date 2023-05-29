26373bbe56c57f4df96c35829fac7361e3f03cb3
whitelion3232 <125092774+whitelion3232@users.noreply.github.com>
2023-05-29 03:23:41 +0200

    Add files via upload

---
+++ ConsoleApp1/Migrations/20230528213220_InitialCreate.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleApp1.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    IsCompany = table.Column<bool>(type: "bit", nullable: false),
+                    TaxIdentificationNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalesInvoices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DocumentNumber = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesInvoices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SalesInvoices_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Elements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(15,2)", nullable: false),
+                    VatRate = table.Column<decimal>(type: "decimal(15,2)", nullable: false),
+                    SalesInvoiceId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Elements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Elements_SalesInvoices_SalesInvoiceId",
+                        column: x => x.SalesInvoiceId,
+                        principalTable: "SalesInvoices",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Elements_SalesInvoiceId",
+                table: "Elements",
+                column: "SalesInvoiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesInvoices_CustomerId",
+                table: "SalesInvoices",
+                column: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Elements");
+
+            migrationBuilder.DropTable(
+                name: "SalesInvoices");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+
+
+// Define the Customer model
+public class Customer
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    [StringLength(2)]
+    public string Country { get; set; }
+
+    public bool IsCompany { get; set; }
+
+    [StringLength(50)]
+    public string TaxIdentificationNumber { get; set; }
+}
+
+// Define the Elements model
+public class Elements
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    public int Quantity { get; set; }
+
+    [Column(TypeName = "decimal(15,2)")]
+    public decimal Price { get; set; }
+
+    [Column(TypeName = "decimal(15,2)")]
+    public decimal VatRate { get; set; }
+}
+
+// Define the SalesInvoice model
+public class SalesInvoice
+{
+    [Key]
+    public int Id { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    [StringLength(20)]
+    public string DocumentNumber { get; set; }
+
+    [ForeignKey("CustomerId")]
+    public Customer Customer { get; set; }
+
+    public List<Elements> Elements { get; set; }
+}
+
+// Define the DbContext
+public class DatabaseContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Elements> Elements { get; set; }
+    public DbSet<SalesInvoice> SalesInvoices { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=WBA_DatabaseConcepts_1;Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true;Integrated Security=True;");
+    }
+}
+
+// Create repositories
+public class SalesInvoiceRepository
+{
+    private readonly DatabaseContext _context;
+
+    public SalesInvoiceRepository(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public void Save(SalesInvoice salesInvoice)
+    {
+        _context.SalesInvoices.Add(salesInvoice);
+        _context.SaveChanges();
+    }
+
+    public List<SalesInvoice> GetAll()
+    {
+        return _context.SalesInvoices.ToList();
+    }
+}
+
+public class CustomerRepository
+{
+    private readonly DatabaseContext _context;
+
+    public CustomerRepository(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public void Save(Customer customer)
+    {
+        _context.Customers.Add(customer);
+        _context.SaveChanges();
+    }
+
+    public List<Customer> GetAll()
+    {
+        return _context.Customers.ToList();
+    }
+}
+
+// Example usage
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new DatabaseContext())
+        {
+            var customerRepository = new CustomerRepository(context);
+            var salesInvoiceRepository = new SalesInvoiceRepository(context);
+
+            // Create instances of models
+            var customer1 = new Customer
+            {
+                Name = "John Doe",
+                Country = "US",
+                IsCompany = false,
+                TaxIdentificationNumber = "12345"
+            };
+
+            var elements1 = new Elements
+            {
+                Name = "Product A",
+                Quantity = 10,
+                Price = 9.99m,
+                VatRate = 0.21m
+            };
+
+            var salesInvoice = new SalesInvoice
+                {
+                    CreatedDate = DateTime.Now,
+                    DocumentNumber = "INV-001",
+                    Customer = customer1,
+                    Elements = new List<Elements> { elements1 }
+                };
+
+            // Save the customer and sales invoice
+            customerRepository.Save(customer1);
+            salesInvoiceRepository.Save(salesInvoice);
+
+            // Retrieve all sales invoices
+            var allSalesInvoices = salesInvoiceRepository.GetAll();
+            foreach (var invoice in allSalesInvoices)
+            {
+                Console.WriteLine($"Invoice ID: {invoice.Id}, Created Date: {invoice.CreatedDate}, " +
+                                  $"Document Number: {invoice.DocumentNumber}, Customer: {invoice.Customer.Name}");
+            }
+        }
+    }
+}
